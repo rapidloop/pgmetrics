724b3a4dd2b4dfc83239370c4208200c4c5a9928
Mahadevan <mahadevan@rapidloop.com>
2022-05-17 15:41:37 +0530

    Update copyright year.

--- collector/system_freebsd.go
+++ collector/system_freebsd.go
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 RapidLoop, Inc.
+ * Copyright 2022 RapidLoop, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
